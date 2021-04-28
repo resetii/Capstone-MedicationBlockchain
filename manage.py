f3a422435da75235996b4ab259c3d38fc843b37f
resetii <jcampion94@gmail.com>
2021-04-27 20:07:03 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
+# SQL Alchemy connects to the Database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 migrate = Migrate(app, db)
